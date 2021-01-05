0402594fa1381bdc3852cf75b55ae53768afb3da
Neil J Thomson <55444453+NeilJ-Thomson@users.noreply.github.com>
2021-01-05 16:28:03 +0000

    Update water_feature_analysis.py

--- water_feature_analysis.py
+++ water_feature_analysis.py
@@ -91,136 +91,127 @@ def local_maxima_3D(data, order=3):
 
     return coords, values
 
-"""
-END OF FUNCTIONS
-"""
+
+def get_water_features(grid_input, structure_input, xtc_input, atom=None, threshold_density=None):
+
+    
+    if atom is None:
+        atomgroup = "OW"
+    ## by default make this average_probability_density
+    
+    
+    
+    
+    u = mda.Universe(structure_input, xtc_input)
+    # ## The density will be obtained from the universe which depends on the .xtc and .gro
+    if grid_input is None:
+        density_atomgroup = u.select_atoms("name " + atomgroup)
+        D = DensityAnalysis(density_atomgroup, delta=1.0)
+        D.run()
+        D.density.convert_density("TIP3P")
+        D.density.export(atomgroup + "_density.dx", type="double")
+        grid_input = atomgroup + "_density.dx"
+    
+    g = Grid(grid_input)
+    # ##converting the density to a probability
+    sol_number = len(u.select_atoms('name ' + atomgroup))
+    grid_data = np.array(g.grid)*sol_number/np.sum(np.array(g.grid))
+    
+    ##can be used to mask all probabilities below the average 
+    average_probability_density = sol_number/np.product(grid_data.shape)
+    if threshold_density is None:
+        threshold_density = average_probability_density
 
 
-## INPUTS
-grid_input = "OW_density.dx"
-structure_input = "na4dkldens.gro"
-xtc_input = "trajforh2ona4dkl.xtc"
-atomgroup = "OW"
-## by default make this average_probability_density
-threshold_density=0.1
+    
+    ##mask all grid centers with density less than threshold density
+    grid_data[grid_data <= threshold_density] = 0.0
+    
+    
+    coords, values = local_maxima_3D(grid_data)
+    
+    maxdens_coord_str = [str(item)[1:-1] for item in coords]
+    
+    # water_pocket_Calphas = [u.select_atoms('name CA and point '+coord+' 5') for coord in maxdens_coord_str]
+    
+    philist=[]
+    psilist=[]
+    
+    counting=[]
+    
+    ###extracting (psi,phi) coordinates for each water dipole specific to the frame they are bound
+    #print('extracting (psi,phi) coordinates for each water dipole specific to the frame they are bound')
+    for i in tqdm(range(len(u.trajectory))):       
+    
+        u.trajectory[i]
+        ##this is where the water pocket is defined as sphere of radius 5 
+        ##centred on centre of geometry of following CA atoms
+        waters_resid=u.select_atoms('resname SOL and point ' + maxdens_coord_str[0]+' 5').resids
+                         
+        ##making a list of water residue IDs for every frame where all three atoms of the water appear in the pocket
+        multi_waters_id=[]
+        for elem in list(set(waters_resid)):
+            if list(waters_resid).count(elem)==3:
+                multi_waters_id.append(elem)
+        counting.append(multi_waters_id)
+    
+    ##making a list of the water IDs that appear in the simulation in that pocket (no dups)
+    flat_list = [item for sublist in counting for item in sublist]
+    no_dups=list(set(flat_list))
+    
+    ###extracting (psi,phi) coordinates for each water dipole specific to the frame they are bound
+    #print('extracting (psi,phi) coordinates for each water dipole specific to the frame they are bound')
+    for i in tqdm(range(len(u.trajectory))):       
+        u.trajectory[i]
+        waters_resid=counting[i]
+        ##extracting the water coordinates based on the water that appears in that frame
+        ##if there is only one water in the pocket then...
+        if len(waters_resid)==1:        
+            ##(x,y,z) positions for the water atom (residue) at frame i
+            water_indices=u.select_atoms('resid ' + str(waters_resid[0])).indices
+            water_atom_positions=u.trajectory[i].positions[water_indices]
+            #print(water_atom_positions)
+            psi, phi = get_dipole(water_atom_positions)
+            psilist.append(psi)
+            philist.append(phi)
+            
+        ##if there are multiple waters in the pocket then find the 
+        ##water that appears in the pocket with the largest frequency and use that 
+        ##ID number to get the coordinate for that frame
+        elif len(waters_resid)>1:
+            
+            freq_count=[]
+            for ID in waters_resid:
+                freq_count.append([flat_list.count(ID),ID])
+            freq_count.sort(key = lambda x: x[0])
+            
+            ##(x,y,z) positions for the water atom (residue) at frame i
+            water_indices=u.select_atoms('resid ' + str(freq_count[-1][1])).indices
+            water_atom_positions=u.trajectory[i].positions[water_indices]
+            #print(water_atom_positions)
+            psi, phi = get_dipole(water_atom_positions)
+            psilist.append(psi)
+            philist.append(phi)
+    
+    
+        ##if there are no waters bound then append these coordinates to identify 
+        ##a separate state
+        elif len(waters_resid)<1:
+            psilist.append(10000.0)
+            philist.append(10000.0)
+            
+    plt.figure()
+    plt.plot(np.histogram([elem for elem in philist if elem !=10000.0],bins=60)[1][0:-1],np.histogram([elem for elem in philist if elem !=10000.0],bins=60)[0],label='D2.50 + Na')
+    plt.xlabel('$\phi$')
+    plt.figure()
+    plt.plot(np.histogram([elem for elem in psilist if elem !=10000.0],bins=60)[1][0:-1],np.histogram([elem for elem in psilist if elem !=10000.0],bins=60)[0],label='D2.50 + Na')
+    plt.xlabel('$\psi$')
+            
+    
+    # filepsi='waterdistributions/'+str(simulation_names)+str(water_names[water_pocket_number])+'_water_distspsi.txt'
+    # filephi='waterdistributions/'+str(simulation_names)+str(water_names[water_pocket_number])+'_water_distsphi.txt'
+    
+    # np.savetxt(filepsi,np.array(psilist))
+    # np.savetxt(filephi,np.array(philist))
 
-
-
-u = mda.Universe(structure_input, xtc_input)
-# ## The density will be obtained from the universe which depends on the .xtc and .gro
-# density_atomgroup = u.select_atoms("name " + atomgroup)
-# D = DensityAnalysis(density_atomgroup, delta=1.0)
-# D.run()
-# D.density.convert_density("TIP3P")
-# D.density.export(atomgroup + "_density.dx", type="double")
-
-
-
-
-g = Grid(grid_input)
-# ##converting the density to a probability
-sol_number = len(u.select_atoms('name ' + atomgroup))
-grid_data = np.array(g.grid)*sol_number/np.sum(np.array(g.grid))
-
-##can be used to mask all probabilities below the average 
-average_probability_density = sol_number/np.product(grid_data.shape)
-if threshold_density is None:
-    threshold_density = average_probability_density
-##mask all grid centers with density less than threshold density
-grid_data[grid_data <= threshold_density] = 0.0
-
-
-coords, values = local_maxima_3D(grid_data)
-
-maxdens_coord_str = [str(item)[1:-1] for item in coords]
-
-# water_pocket_Calphas = [u.select_atoms('name CA and point '+coord+' 5') for coord in maxdens_coord_str]
-
-philist=[]
-psilist=[]
-
-counting=[]
-
-###extracting (psi,phi) coordinates for each water dipole specific to the frame they are bound
-#print('extracting (psi,phi) coordinates for each water dipole specific to the frame they are bound')
-for i in tqdm(range(len(u.trajectory))):       
-
-    u.trajectory[i]
-    ##this is where the water pocket is defined as sphere of radius 5 
-    ##centred on centre of geometry of following CA atoms
-    waters_resid=u.select_atoms('resname SOL and point '+maxdens_coord_str[0]+' 5').resids
-                     
-    ##making a list of water residue IDs for every frame where all three atoms of the water appear in the pocket
-    multi_waters_id=[]
-    for elem in list(set(waters_resid)):
-        if list(waters_resid).count(elem)==3:
-            multi_waters_id.append(elem)
-    counting.append(multi_waters_id)
-
-##making a list of the water IDs that appear in the simulation in that pocket (no dups)
-flat_list = [item for sublist in counting for item in sublist]
-no_dups=list(set(flat_list))
-
-###extracting (psi,phi) coordinates for each water dipole specific to the frame they are bound
-#print('extracting (psi,phi) coordinates for each water dipole specific to the frame they are bound')
-for i in tqdm(range(len(u.trajectory))):       
-    u.trajectory[i]
-    waters_resid=counting[i]
-    ##extracting the water coordinates based on the water that appears in that frame
-    ##if there is only one water in the pocket then...
-    if len(waters_resid)==1:        
-        ##(x,y,z) positions for the water atom (residue) at frame i
-        water_indices=u.select_atoms('resid ' + str(waters_resid[0])).indices
-        water_atom_positions=u.trajectory[i].positions[water_indices]
-        #print(water_atom_positions)
-        psi, phi = get_dipole(water_atom_positions)
-        psilist.append(psi)
-        philist.append(phi)
-        
-    ##if there are multiple waters in the pocket then find the 
-    ##water that appears in the pocket with the largest frequency and use that 
-    ##ID number to get the coordinate for that frame
-    elif len(waters_resid)>1:
-        
-        freq_count=[]
-        for ID in waters_resid:
-            freq_count.append([flat_list.count(ID),ID])
-        freq_count.sort(key = lambda x: x[0])
-        
-        ##(x,y,z) positions for the water atom (residue) at frame i
-        water_indices=u.select_atoms('resid ' + str(freq_count[-1][1])).indices
-        water_atom_positions=u.trajectory[i].positions[water_indices]
-        #print(water_atom_positions)
-        psi, phi = get_dipole(water_atom_positions)
-        psilist.append(psi)
-        philist.append(phi)
-
-
-    ##if there are no waters bound then append these coordinates to identify 
-    ##a separate state
-    elif len(waters_resid)<1:
-        psilist.append(10000.0)
-        philist.append(10000.0)
-        
-plt.figure()
-plt.plot(np.histogram([elem for elem in philist if elem !=10000.0],bins=60)[1][0:-1],np.histogram([elem for elem in philist if elem !=10000.0],bins=60)[0],label='D2.50 + Na')
-plt.xlabel('$\phi$')
-plt.figure()
-plt.plot(np.histogram([elem for elem in psilist if elem !=10000.0],bins=60)[1][0:-1],np.histogram([elem for elem in psilist if elem !=10000.0],bins=60)[0],label='D2.50 + Na')
-plt.xlabel('$\psi$')
-        
-# filepsi='waterdistributions/'+str(simulation_names)+str(water_names[water_pocket_number])+'_water_distspsi.txt'
-# filephi='waterdistributions/'+str(simulation_names)+str(water_names[water_pocket_number])+'_water_distsphi.txt'
-
-# np.savetxt(filepsi,np.array(psilist))
-# np.savetxt(filephi,np.array(philist))
-
-
-
-
-
-
-
-
-
-
+    return psilist, philist
